92b52f7a556191ab6a3f15216165dd4f0f986cd5
Nikita <73963537+Ushakek@users.noreply.github.com>
2021-04-30 00:19:23 +0500

    Update tictaktoe.py

--- tictaktoe.py
+++ tictaktoe.py
@@ -9,31 +9,36 @@ def print_board(pos):
 def check_win(win):
     win_pos = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
                (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-
-    for i in win_pos:
-        if win[i[0]] == win[i[1]] == win[i[2]]:
-            return win[i[0]]
-    return False
-
-
-def check_game(pos):
     some_x = []
     some_o = []
+    count = 0
 
-    for i in pos:
+    for i in win:
         if i == 'X':
             some_x.append(i)
         elif i == 'O':
             some_o.append(i)
-    if len(pos) != (len(some_o) + len(some_x)):
-        return 'Game not finished'
+
+    if (len(some_x) > (len(some_o) + 1) or len(some_o) > (len(some_x) + 1)) and len(some_x) != len(some_o):
+        return print('Impossible')
+
+    for i in win_pos:
+        if win[i[0]] == win[i[1]] == win[i[2]]:
+            count += 1
+    if count > 1:
+        return print('Impossible')
+
+    for i in win_pos:
+        if win[i[0]] == win[i[1]] == win[i[2]]:
+            return print('{} wins'.format(win[i[0]]))
+
+    if len(win) != (len(some_o) + len(some_x)):
+        return print('Game not finished')
+
+    if len(some_x) + len(some_o) == 9:
+        return print('Draw')
 
 
 user_input = input()
 print_board(user_input)
-who_win = check_win(user_input)
-
-if not who_win:
-    print(check_game(user_input))
-else:
-    print('{} wins'.format(who_win))
+check_win(user_input)
